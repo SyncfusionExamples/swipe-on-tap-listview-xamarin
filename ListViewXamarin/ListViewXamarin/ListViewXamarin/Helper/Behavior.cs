e692a0eeeb613e5a32983f2fee4b9af75ae571d0
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-09-08 17:43:40 +0530

    XAMARIN-36916 - Sample and screen shot added

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+
+        SfListView ListView;
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("listView");
+            ListView.ItemTapped += ListView_ItemTapped;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            ListView.ItemTapped -= ListView_ItemTapped;
+            ListView = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region CallBack
+        private void ListView_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
+        {
+            ListView.SwipeItem(e.ItemData, ListView.SwipeOffset);
+            e.Handled = true;
+        }
+        #endregion
+    }
+}
